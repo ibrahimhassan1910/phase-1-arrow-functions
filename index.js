90c1eccec219ed80e3a9f0cd26236627900af40c
hassan <hassanibrahimndahiro@gmail.com>
2024-10-09 21:05:02 +0300

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+// function foo (){
+//     console.log('bar');   
+// }
+// foo();
+
+// const foo = function(){
+//     console.log('bar');
+// }
+// foo();
+
+// function expression called divide
+const divide = function(a, b){
+    return a/b;
+}
+
+// An arrow function called square
+const square = function(x,){
+    return x * x;
+}
+
+//An arroww function called add
+
+const add = function (a, b){
+    return a + b;
+}
